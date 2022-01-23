880706237f37e6c7292c048144d082d116a0e363
Joslyn <joslynsipod@gmail.com>
2022-01-22 20:07:58 -0800

    commit tree d2b3b473f1f7fc9d6e9619f402eec6a2601a28d5

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(records) {
+    const found = records.find(function(year) {
+        return year.result === "W";
+    });
+    // return !!found ? found.year : undefined ;
+    if (found != undefined) {
+        return found.year
+    }else {
+        return undefined 
+    }
+}
